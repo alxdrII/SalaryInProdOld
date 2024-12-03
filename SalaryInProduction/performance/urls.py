b20d97e52031c4c3f7f5b8168fa18eecf2bd2548
lebedevaa <forlebedeva@mail.ru>
2024-12-03 20:59:49 +0300

    03.12.2024 20:59 links

--- SalaryInProduction/performance/urls.py
+++ SalaryInProduction/performance/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', perf_main),
-    path('doc_show/<int:doc_id>', perf_doc_show),
+    path('', perf_main, name='docs_list'),
+    path('doc_show/<int:doc_id>', perf_doc_show, name='perf_doc'),
     path('dicts/works/', works_view, name="employees_list"),
     path('dicts/prods/', prods_view, name="products_list"),
-    path('dicts/', dicts_view),
-    path('report/', report_view),
-    path('edit_empl/<int:empl_id>/', edit_employee),
-    path('edit_prod/<int:prod_id>/', edit_product),
+    path('dicts/', dicts_view, name='menu_lists'),
+    path('report/', report_view, name='report'),
+    path('edit_empl/<int:empl_id>/', edit_employee, name='edit_employee'),
+    path('edit_prod/<int:prod_id>/', edit_product, name='edit_product'),
 ]
